ef7d6fcaa8b6554081f88dc972e76dc14b752c99
qiangyujun <qiangyujun@jd.com>
2021-11-25 11:21:20 +0800

    创建项目，引入vant ui

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入 vant 
+import '@/vantui'
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/vantui/index.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import 'vant/lib/button/style';
+import { Button } from 'vant';
+Vue.use(Button);
